def4ead7dd9fd0179a2d8432b9c901563dbc4252
XMelexus <58857939+XMelexus@users.noreply.github.com>
2020-11-07 16:32:43 +0300

    ншпшпн

---
+++ AVITO/Announcement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AVITO
+{
+    public class Announcement
+    {
+        private StackPanel panel;
+        private Image image;
+        private TextBlock labelName;
+        private TextBlock labelPrice;
+        private TextBlock labelPlace;
+        private TextBlock labelCreateDate;
+
+        public Announcement(string name, double price, string place, string dateOfCreate)
+        {
+            panel = new StackPanel()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(5)
+            };
+            image = new Image();
+            // Картинка не реализована
+
+            labelName = CreateTextBlock(17, new SolidColorBrush((Color)ColorConverter.ConvertFromString("#009cf0")), FontWeights.Bold);
+            labelName.Text = name;
+
+            labelPrice = CreateTextBlock(20, Brushes.Gray, FontWeights.Bold);
+            labelPrice.Text = price.ToString();
+
+            labelPlace = CreateTextBlock(17, Brushes.Gray, FontWeights.Medium);
+            labelPlace.Text = place;
+
+            labelCreateDate = CreateTextBlock(17, Brushes.Gray, FontWeights.Medium);
+            labelCreateDate.Text = dateOfCreate;
+
+        }
+
+        private TextBlock CreateTextBlock(double fontSize, Brush foreGround, FontWeight fontWeight)
+        {
+            return new TextBlock()
+            {
+                FontSize = fontSize,
+                Foreground = foreGround,
+                FontWeight = fontWeight,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                TextWrapping = TextWrapping.Wrap
+            };
+        }
+
+    }
+}
---
+++ AVITO/ImageJSON.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Media.Imaging;
+
+namespace AVITO
+{
+    public class ImageJSON
+    {
+        public byte[] ImageBytes { set; get; }
+
+
+        public BitmapImage GetBitmapImage()
+        {
+            if (ImageBytes == null || ImageBytes.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(ImageBytes))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
